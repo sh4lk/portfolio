68e45e97a7b7af15369460c39b5f6fef1f04391b
sh4lk <mateoadnot@gmail.com>
2025-11-19 23:14:44 +0100

    grosse modif

--- particules.js
+++ particules.js
@@ -4,3 +4,19 @@ window.addEventListener('scroll', function() {
   var scrolled = (scrollTop / docHeight) * 100;
   document.getElementById('progress-bar').style.width = scrolled + '%';
 });
+
+// experience-slider.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  const leftBtn = document.querySelector('.exp-arrow.left');
+  const rightBtn = document.querySelector('.exp-arrow.right');
+  const slider = document.querySelector('.exp-slider');
+
+  leftBtn.addEventListener('click', function() {
+    slider.scrollBy({ left: -320, behavior: 'smooth' });
+  });
+
+  rightBtn.addEventListener('click', function() {
+    slider.scrollBy({ left: 320, behavior: 'smooth' });
+  });
+});
